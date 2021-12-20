851308cb0290956550e6e0ab77db8be19a14de70
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2021-12-21 01:16:40 +0900

    Fix DangerSwiftShoki URL
    I forgot to change the url from old closed ssh address to new open https address

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "3.0.0"),
         .package(name: "DangerSwiftHammer", url: "https://github.com/el-hoshino/DangerSwiftHammer", from: "0.2.0"),
-        .package(name: "DangerSwiftShoki", url: "git@github.com:yumemi/danger-swift-shoki.git", from: "0.2.0"),
+        .package(name: "DangerSwiftShoki", url: "https://github.com/yumemi-inc/danger-swift-shoki/", from: "0.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
