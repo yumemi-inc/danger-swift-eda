5d58c886f1fd74e81679609f982c2799816840e5
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-11-17 00:22:14 +0900

    Add APIs to quickly create a workflow

--- Sources/DangerSwiftEda/PRWorkflow.swift
+++ Sources/DangerSwiftEda/PRWorkflow.swift
@@ -10,3 +10,19 @@ import DangerSwiftShoki
 public protocol PRWorkflow {
     func doWorkflowCheck(against info: PRMetaData, using utility: PRUtility) throws -> Report
 }
+
+public extension PRWorkflow where Self == GitFlow {
+    
+    static func gitFlow(configuration: GitFlow.Configuration = .default) -> Self {
+        GitFlow(configuration: configuration)
+    }
+    
+}
+
+public extension PRWorkflow where Self == GitHubFlow {
+    
+    static func gitHubFlow(configuration: GitHubFlow.Configuration = .default) -> Self {
+        GitHubFlow(configuration: configuration)
+    }
+    
+}
